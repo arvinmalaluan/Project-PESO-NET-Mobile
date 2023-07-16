5d8f116ced2a0c7f52951d97b3253ebb01ef8e78
Arvin Malaluan <113675340+arvinmalaluan@users.noreply.github.com>
2023-07-17 06:02:18 +0800

    jobs - 50%

--- pesonet-client-side/src/app/components/navbar/navData.ts
+++ pesonet-client-side/src/app/components/navbar/navData.ts
@@ -2,26 +2,26 @@ export const navData = [
   {
     id: 1,
     title: 'Home',
-    path: 'home',
+    path: '/home',
   },
   {
     id: 2,
     title: 'Jobs',
-    path: 'jobs',
+    path: '/jobs',
   },
   {
     id: 3,
     title: 'Messages',
-    path: 'messages',
+    path: '/messages',
   },
   {
     id: 4,
     title: 'Notifications',
-    path: 'notifications',
+    path: '/notifications',
   },
   {
     id: 5,
     title: 'Profile',
-    path: 'manage-post',
+    path: '/manage-post',
   },
 ];
